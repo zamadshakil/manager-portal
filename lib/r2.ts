01db7ffdd209dd2615a477c3ad0b08953ce59164
Zamad Shakeel <zamadshakil@gmail.com>
2026-05-03 23:55:28 +0500

    jfjhc

--- lib/r2.ts
+++ lib/r2.ts
@@ -7,6 +7,7 @@ const r2 = new S3Client({
     accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
   },
+  forcePathStyle: true,
 })
 
 const BUCKET = process.env.R2_BUCKET_NAME || ""
